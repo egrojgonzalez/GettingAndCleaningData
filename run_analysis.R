210ebf61282f1bbc28882b974da4ff6b381e2d4f
Jorge G Navarrete <jorgenavarrete@bell.net>
2014-08-19 23:45:42 -0400

    commit on Aug 19

--- run_analysis.R
+++ run_analysis.R
@@ -8,13 +8,13 @@
 
 # This script will perform the following steps on the UCI HAR Dataset downloaded from 
 # https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
+
 # 1. Merge the training and the test sets to create one data set.
 # 2. Extract only the measurements on the mean and standard deviation for each measurement. 
 # 3. Use descriptive activity names to name the activities in the data set
 # 4. Appropriately label the data set with descriptive activity names. 
 # 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
 
-##########################################################################################################
 # assume datasets are downloaded and unziped in "Project/UCI HAR Dataset" folder
 
 setwd ("C:/COURSERA/Getdata/Project")
@@ -32,8 +32,9 @@ if (!require("reshape2")) {
 require("data.table") 
 require("reshape2") 
 
-#Step 1: "Merges the training and the test sets to create one data set." 
+################################################################################################
 
+#Step 1: "Merges the training and the test sets to create one data set." 
 
  # read subject training data 
  subject_train = read.table("UCI HAR Dataset/train/subject_train.txt", col.names=c("subject_id")) 
